033846f26f9228ba653a6d24dd0582d782ba9e02
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2024-06-04 21:53:48 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,14 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from datetime import date, datetime 
+import pytz
+from aiohttp import web
+from plugins import web_server
 
 class Bot(Client):
 
@@ -42,6 +46,10 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
